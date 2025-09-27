6e044c262fa4c4e58b8e9c3248931da2f910cbd9
sultanalkhoory <sultanalkhoory@hotmail.com>
2025-09-27 22:47:00 +0400

    Update IPhoneFrame.tsx

--- components/IPhoneFrame.tsx
+++ components/IPhoneFrame.tsx
@@ -1,16 +1,29 @@
 'use client';
-import { motion } from "framer-motion";
-import { scaleIn } from "@/lib/animations";
+import { motion } from 'framer-motion';
+import { scaleIn } from '@/lib/animations';
 
 export default function IPhoneFrame({ children }: { children: React.ReactNode }) {
   return (
-    <motion.div variants={scaleIn} initial="hidden" animate="show" className="relative inline-block" style={{ width: 320 }}>
-      {/* Screen area: adjust paddings to align with your bezel asset */}
-      <div className="absolute" style={{ left: 20, right: 20, top: 24, bottom: 24, borderRadius: 24, overflow: 'hidden' }}>
-        {children}
+    <motion.div
+      variants={scaleIn}
+      initial="hidden"
+      animate="show"
+      className="relative inline-block"
+      style={{ width: 320 }}
+    >
+      {/* Device outer frame */}
+      <div className="relative aspect-[9/19.5] rounded-[42px] bg-black shadow-iphone border-[6px] border-black overflow-hidden">
+        {/* Inner screen */}
+        <div className="absolute inset-0 rounded-[36px] bg-black">
+          {/* Notch / Dynamic Island */}
+          <div className="absolute top-[10px] left-1/2 -translate-x-1/2 w-[110px] h-[32px] rounded-full bg-black" />
+
+          {/* Screen content */}
+          <div className="absolute top-[46px] bottom-[18px] left-[8px] right-[8px] rounded-[30px] overflow-hidden bg-black">
+            {children}
+          </div>
+        </div>
       </div>
-      {/* Bezel image (you will add /public/iphone-17pro.png) */}
-      <img src="/iphone-17pro.png" alt="iPhone 17 Pro" className="block w-full h-auto drop-shadow-2xl shadow-iphone select-none pointer-events-none" />
     </motion.div>
   );
 }
