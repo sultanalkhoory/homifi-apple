b67f7ca0aa9a05d3bab9546cb00b8a61a20cfcf9
sultanalkhoory <sultanalkhoory@hotmail.com>
2025-09-27 22:56:57 +0400

    Update IPhoneFrame.tsx

--- components/IPhoneFrame.tsx
+++ components/IPhoneFrame.tsx
@@ -6,7 +6,8 @@ import { scaleIn } from '@/lib/animations';
  * Apple-accurate CSS iPhone frame
  * - Proper bezel thickness & radii
  * - Correct Dynamic Island size/position
- * - Screen content is perfectly clipped to the inner rounded rectangle
+ * - Side buttons (volume, mute, power)
+ * - Screen content is clipped to inner rounded rectangle
  * - Responsive widths (mobile → desktop)
  */
 export default function IPhoneFrame({ children }: { children: React.ReactNode }) {
@@ -25,11 +26,23 @@ export default function IPhoneFrame({ children }: { children: React.ReactNode })
         <div className="pointer-events-none absolute inset-0 rounded-[40px]
                         ring-1 ring-white/10 shadow-[inset_0_0_6px_rgba(255,255,255,0.12)]" />
 
-        {/* Inner bezel (the thin black border around the screen) */}
+        {/* === Side Buttons === */}
+
+        {/* Left side - Volume Up */}
+        <div className="absolute -left-[3px] top-[80px] w-[4px] h-[40px] rounded-l-md bg-neutral-800 shadow-md" />
+        {/* Left side - Volume Down */}
+        <div className="absolute -left-[3px] top-[140px] w-[4px] h-[40px] rounded-l-md bg-neutral-800 shadow-md" />
+        {/* Left side - Mute switch */}
+        <div className="absolute -left-[3px] top-[40px] w-[4px] h-[28px] rounded-l-md bg-neutral-700 shadow-sm" />
+
+        {/* Right side - Power button */}
+        <div className="absolute -right-[3px] top-[100px] w-[4px] h-[70px] rounded-r-md bg-neutral-800 shadow-md" />
+
+        {/* Inner bezel (thin black border around screen) */}
         <div className="absolute inset-[6px] rounded-[34px] bg-black
                         shadow-[inset_0_0_10px_rgba(0,0,0,0.6)]">
 
-          {/* Dynamic Island (accurate size & placement) */}
+          {/* Dynamic Island */}
           <div className="absolute left-1/2 -translate-x-1/2
                           top-[10px] sm:top-[10px] md:top-[12px]
                           w-[92px] sm:w-[100px] md:w-[108px] lg:w-[118px]
@@ -37,7 +50,7 @@ export default function IPhoneFrame({ children }: { children: React.ReactNode })
                           rounded-full bg-black
                           shadow-[inset_0_-1px_2px_rgba(255,255,255,0.12)]" />
 
-          {/* Screen (content is clipped here) */}
+          {/* Screen (clipped area) */}
           <div
             className="
               absolute
