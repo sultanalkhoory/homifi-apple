f6406cdf35dcd0645802702e3eaff5b0106f520e
sultanalkhoory <sultanalkhoory@hotmail.com>
2025-09-27 22:47:28 +0400

    Update IPhoneFrame.tsx

--- components/IPhoneFrame.tsx
+++ components/IPhoneFrame.tsx
@@ -11,16 +11,27 @@ export default function IPhoneFrame({ children }: { children: React.ReactNode })
       className="relative inline-block"
       style={{ width: 320 }}
     >
-      {/* Device outer frame */}
-      <div className="relative aspect-[9/19.5] rounded-[42px] bg-black shadow-iphone border-[6px] border-black overflow-hidden">
-        {/* Inner screen */}
-        <div className="absolute inset-0 rounded-[36px] bg-black">
-          {/* Notch / Dynamic Island */}
-          <div className="absolute top-[10px] left-1/2 -translate-x-1/2 w-[110px] h-[32px] rounded-full bg-black" />
+      {/* Outer device frame */}
+      <div className="relative aspect-[9/19.5] rounded-[42px] bg-black shadow-iphone overflow-hidden">
 
-          {/* Screen content */}
+        {/* Metallic rim highlight */}
+        <div className="absolute inset-0 rounded-[42px] border-[2px] border-gray-700/60 shadow-[inset_0_0_4px_rgba(255,255,255,0.15)]" />
+
+        {/* Inner bezel */}
+        <div className="absolute inset-[6px] rounded-[36px] bg-black shadow-[inset_0_0_6px_rgba(0,0,0,0.6)]">
+
+          {/* Dynamic Island / notch */}
+          <div className="absolute top-[10px] left-1/2 -translate-x-1/2 w-[110px] h-[32px] rounded-full bg-black shadow-[inset_0_-1px_2px_rgba(255,255,255,0.12)]" />
+
+          {/* Screen with gloss overlay */}
           <div className="absolute top-[46px] bottom-[18px] left-[8px] right-[8px] rounded-[30px] overflow-hidden bg-black">
-            {children}
+            <div className="relative w-full h-full">
+              {/* Screen content */}
+              {children}
+
+              {/* Subtle glossy reflection overlay */}
+              <div className="absolute inset-0 bg-gradient-to-b from-white/10 via-transparent to-black/10 pointer-events-none" />
+            </div>
           </div>
         </div>
       </div>
